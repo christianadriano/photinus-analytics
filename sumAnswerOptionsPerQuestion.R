f522c53421a28a5a1a6b2bcf32da5bcdbf12e19c
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-07-31 23:17:23 +0200

    data to compute answer aggregation

--- sumAnswerOptionsPerQuestion.R
+++ sumAnswerOptionsPerQuestion.R
@@ -23,24 +23,44 @@ countAnswerOptions<- function(dataf){
   question_by <- group_by(subset_df,QuestionID);
   summaryTable<- summarize(question_by,TotalYes = sum(YesCount),TotalNo = sum(NoCount), TotalIDK = sum(IDKCount));
   
-  colnames(summaryTable)<-c("Questoin.ID","Yes.Count","No.Count","IDK.Count");
+  colnames(summaryTable)<-c("Question.ID","Yes.Count","No.Count","IDK.Count");
   
   return(summaryTable);
 }
 
 
-computeMajorityVote(summaryTable){
+computeMajorityVote<- function(summaryTable){
   summaryTable["MajorityVote"] <- summaryTable$Yes.Count - summaryTable$No.Count;
   return(summaryTable);
 }
-  
-computeThreshold(summaryTable, threshold){
+
+computeThreshold<- function(summaryTable, threshold){
   summaryTable["ThresholdVote"] <- summaryTable[,"Yes.count"]>threshold;
   return(summaryTable);
 }
-  
-computeRanking(summaryTable, threshold){
+
+#Provides the list of questions that have to be considered 
+#and the list of questions that cover bugs
+computeRanking<- function(summaryTable,  questionList){
   summaryTable["ThresholdVote"] <- summaryTable[,"Yes.count"]>threshold;
   return(summaryTable);
 }
-  
+
+#Add a column with the information of whether the question.ID 
+#covers a bug of not.
+appendGroundTruth<- function(summaryTable, questionList){
+  list <- summaryTable[,"Question.ID"];
+  summaryTable[,"bugCovering"] <- list$Question.ID %in% questionList;
+  return(summaryTable);  
+}
+
+questionList <- c(1,4,10,14,20,23,30,32,55,56,57,58,59,72,73,77,84,92,95,97,102,104,115,119,123);
+JavaMethod1_questions <- c(1:9);
+JavaMethod2_questions <- c(10:15); 
+JavaMethod3_questions <- c(16:32);
+JavaMethod4_questions <- c(33:69);
+JavaMethod5_questions <- c(70:78);
+JavaMethod6_questions <- c(79:96);
+JavaMethod7_questions <- c(97:104);
+JavaMethod8_questions <- c(105:128);
+
