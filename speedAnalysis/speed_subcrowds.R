71d8b952522664a11526a77b2a8b9754aa0254d7
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2018-09-03 12:03:36 +0200

    Documentation of design decisions

--- speedAnalysis/speed_subcrowds.R
+++ speedAnalysis/speed_subcrowds.R
@@ -1,1 +1,23 @@
+
 #Compute speed of subcrowds
+
+#Programmers performed up to three microtasks for each software failure resolution task. 
+#Therefore, microtasks performed by the same programmer, were necessarily done in a sequence. We called these sequences execution streaks.
+#To obtain the time to execute all the microtasks for one failure resolution task, we just need to compute the longest streak.
+#We also computed the qualification time, which is the time to for each programmer to provide consent, answer the survey, and take the qualification test.
+#This way, we compute all the time that is affected by our method. Time to recruit enough workers is outside our method.
+#We tried to positively influence the speed of recruitment with a set measures. We made initial posting when US population is awake (6 am PCT).
+#We provided appropriate intrinsic motivation by paying the equivalent to the minimal wage in California (9 dollars/hour).
+#We described the study in a way to sensibilize participants on the importance of the study (extrinsic motivation)
+#However, in the second experiment, we did not announce the tasks in MTurk forums, this way we tried to mitigate bias on our measurements of
+#the recruitment speed on MT. 
+#Later on the paper we evaluate the outcome of these measures.
+
+#Note that the design of experiment-1 had longer streaks because each programmer was offered ten microtasks that had to be performed in a sequence.
+
+#Workforce concentration
+#Moreover, we posted microtasks for all eight failure resolutions at the same time. Therefore, we did not optimize to have all programmers focused on one failure resolution at a time. 
+#In practical situation, an agile team would prefer to have one failure resolution ready before giving a new task to the crowd. This way
+#the agile team can be working on a fix while the crowd works on a new failure resolution.
+
+#Obtain 
